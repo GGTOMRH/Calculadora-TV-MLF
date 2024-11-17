3fb256c944ef0e5b330bfdcd914c4c0906029dfd
Tomás Varzim <129333443+GGTOMRH@users.noreply.github.com>
2024-11-17 23:31:46 +0000

    Add files via upload

--- Calculadora avançada.py
+++ Calculadora avançada.py
@@ -1,6 +1,43 @@
 import time
 import random
 
+def main():
+  load()
+  while True:
+    try:
+      calculo = input()
+      if calculo == "stop":
+        break
+      elif calculo == "help":
+        calc_help()
+        continue
+      resultado = matematica(calculo)
+      print(resultado)
+    except ValueError:
+      print("Syntax Error")
+    except ZeroDivisionError as e:
+          print(e)
+
+def load():
+  print("A inciar calculadora TV-MLF")
+  time.sleep(0.5)
+  load = 0
+  while load < 100:
+    if load > 95:
+      load += 1
+    elif load > 90:
+      load += random.randint(1, 5)
+    else:
+      load += random.randint(5, 10)
+    print("loading", str(load)+"%")
+    time.sleep(0.1)
+  time.sleep(0.5)
+  print("Bem-vindo à TV-MLF :) ")
+
+def calc_help():
+  print("Escolhe o tipo de ajuda: ")
+
+
 def matematica(n):
   for operator in "+-*/^":
         index = n.find(operator)
@@ -11,46 +48,18 @@ def matematica(n):
             numero_resto = matematica(resto)
             
             if operator == "+":
-                return int(numero) + numero_resto
+                return float(numero) + numero_resto
             elif operator == "-":
-                return int(numero) - numero_resto
+                return float(numero) - numero_resto
             elif operator == "*":
-                return int(numero) * numero_resto
+                return float(numero) * numero_resto
             elif operator == "/":
-                if int(numero_resto) == 0:
+                if float(numero_resto) == 0:
                     raise ZeroDivisionError("Math Error")
-                return int(numero) // numero_resto
+                return float(numero) / numero_resto
             elif operator == "^":
-              return int(numero) ** numero_resto
+              return float(numero) ** numero_resto
 
-  return int(n)
+  return float(n)
 
-#iniciar a calculador
-print("A inciar calculadora TV-MLF")
-load = 0
-while load < 100:
-  if load > 95:
-    load += 1
-  elif load > 90:
-    load += random.randint(1, 5)
-  else:
-    load += random.randint(5, 10)
-  print("loading", str(load)+"%")
-  time.sleep(0.1)
-print("Bem-vindo à TV-MLF :) ")
-
-#input da conta
-while True:
-  try:
-    calculo = input()
-    if calculo == "stop":
-      break
-    if calculo == "help":
-      print("")
-    matematica(calculo)
-    resultado = matematica(calculo)
-    print(resultado)
-  except ValueError:
-    print("Syntax Error")
-  except ZeroDivisionError as e:
-        print(e)
+main()
