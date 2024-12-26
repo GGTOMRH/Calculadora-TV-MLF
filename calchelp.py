05ec92ee4ada142b28617c710b0b18150ae70954
Tomás Varzim <129333443+GGTOMRH@users.noreply.github.com>
2024-12-26 15:00:17 +0000

    caclhep acabado

--- calchelp.py
+++ calchelp.py
@@ -5,7 +5,7 @@ def calc_help():
   helps: list[str] = ["Simbolos", "Formulas", "Calculadora", "Outro"]
   print("Escolhe o tipo de ajuda: ")
   for i, ajuda in enumerate(helps, start=1):
-    print(i, ajuda)
+    print(f"{i}. {ajuda}")
   while True:
     ajuda: str = input().capitalize()
     if ajuda == "Simbolos" or ajuda == "1":
@@ -18,16 +18,17 @@ def calc_help():
       print("A # representa raiz quadrada")
       break
     elif ajuda == "Formulas" or ajuda == "2":
-      print("Escolhe a area das formulas: ")
       formulas()
       break
     elif ajuda == "Calculadora" or ajuda == "3":
-      print("A calculadora TV-MLF é o primeiro")
+      print("A calculadora TV-MLF é o primeiro modelo da cortex corporation.")
+      print("Ela funciona de uma forma muito simples, ao colocar as contas no terminal,")
+      print("Ela executa o nosso programa secreto\n")
+      print("Para além disso ela vem com formulas especiais no seu codigo")
+      print("Espero que goste do nosso produto!")
       break
     elif ajuda == "Outro" or ajuda == "4":
       print("\nNão sabemos como ajudá-lo! \nQualquer questão entre em contacto através do nosso email:\ncortexcorporationpt@gmail.com\n")
       break
     else:
       print("Não encontrei a tua ajuda, tenta denovo!")
-
-calc_help()
