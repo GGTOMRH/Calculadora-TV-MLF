cea5962be649f0122344f0c6cbcca7eab12a36c7
Tomás Varzim <129333443+GGTOMRH@users.noreply.github.com>
2025-01-12 00:09:51 +0000

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -2,11 +2,50 @@ import math as m
 
 pi = m.pi
 
+formulasFundamentais = ["Seno", "Cosseno", "Tangente"]
+
+
 def teorema_de_pitagoras():
   x = input("Primeiro cateto: ")
   y = input("Segundo cateto: ")
   h = (float(x) ** 2 + float(y) ** 2) ** 0.5
   print(f"A hipotenusa é {h}")
+
+def distancia():
+  x1 = input("X do primeiro ponto:")
+  y1 = input("Y do primeiro ponto:")
+  x2 = input("X do segundo ponto:")
+  y2 = input("Y do segundo ponto:")
+  distancia = m.sqrt(((float(x1) - float(x2))**2) + ((float(y1) - float(y2))**2))
+  print(f"A distancia entre os pontos é {distancia}")
+
+def formulas_fundamentais():
+  print("Escolhe uma razão trignometrica:")
+  for i, formula in enumerate(formulasFundamentais, start=1):
+    print(f"{i}. {formula}")
+    
+  while True:
+      y = input()
+      if y == "1" or y == "Seno":
+        seno = input("Qual é o seno?:")
+        cos = m.sqrt(1 - (float(seno) ** 2))
+        tg = float(seno)/float(cos)
+        print(f"Cosseno:{cos}\nTangente:{tg}")
+        break
+      elif y == "2" or y == "Cosseno":
+        cos = input("Qual é o cosseno?:")
+        seno = m.sqrt(1 - (float(cos) ** 2))
+        tg = float(seno)/float(cos)
+        print(f"Seno:{seno}\nTangente:{tg}")
+        break
+      elif y == "3" or y == "Tangente":
+        tg = input("Qual é a tangente?:")
+        cos = m.sqrt(1/((float(tg) ** 2) + 1))
+        sen = float(tg) * float(cos)
+        print(f"Cosseno:{cos}\nSeno:{sen}")
+        break
+      else:
+        print("Não encontrei essa razão trignometrica! tenta denovo.")
 
 def seno():
   alpha = input("Angulo em rad: ")
